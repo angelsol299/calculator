13502e886aee66fb5d3822a81dd4a237fd791cca
angelsol299 <angelsol299@hotmail.com>
2019-01-10 17:36:48 +0100

    Button js logic 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="calc-wrapper">test</div>
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+        </div>
       </div>
     );
   }
