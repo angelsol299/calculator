c5b5e95b2fbdaa1ea56b1a5875daf6a27567e7b6
angelsol299 <angelsol299@hotmail.com>
2019-01-10 17:31:40 +0100

    Button js logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Button.css";
 
-const Button = props => <div>{props.children}</div>;
+const isOperator = val => {
+  return !isNaN(val) || val === "." || val === "=";
+};
+
+const Button = props => (
+  <div
+    className={`button-wrapper ${
+      isOperator(props.children) ? null : "operator"
+    }`}
+  >
+    {props.children}
+  </div>
+);
 
 export default Button;
